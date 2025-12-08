3b6d58a6bd2ce82953d28c63ee772b6725ca3c5f
LucaGiu204 <lucagiu204@gmail.com>
2025-12-08 16:46:09 -0300

    e.e

--- obj/Debug/net8.0/Patata.AssemblyInfo.cs
+++ obj/Debug/net8.0/Patata.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patata")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f94fe53661c9113cc4e504447737c8033cd77eef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4585fe36528466a11a892222d2d89b2a31a0ce8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patata")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patata")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
